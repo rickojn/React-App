6191fec0e6bf26e09969b7b97aa742b2e3a061cd
unknown <rickojn@hotmail.com>
2018-09-10 22:33:08 +0100

    add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import { configure } from 'enzyme';
+import {expect} from 'chai'
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+
+it('use Enzyme', () => {
+  const wrapper = shallow(<App/>);
+  console.log('logging ...', wrapper.find('TopBit').getElements())
+  expect(wrapper.find('div')).to.have.lengthOf(1)
 });
